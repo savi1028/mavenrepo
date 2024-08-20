434d23b2b5a73ec231b7ece980a7b3a6fe0cb86a
savi <savithah9845@gmail.com>
2024-08-20 05:20:44 -0700

    java project

---
+++ java1/src/main/java/savi1028/App.java
@@ -0,0 +1,13 @@
+package savi1028;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
